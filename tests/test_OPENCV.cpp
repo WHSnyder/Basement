c8ea6276f41fa310695bfbb05139e557c3d22214
William Herald Snyder <will.herald.snyder@gmail.com>
2020-03-06 09:04:47 -0500

    Basic cmake working very well

--- tests/test_OPENCV.cpp
+++ tests/test_OPENCV.cpp
@@ -1,21 +1,25 @@
+#include <pybind11/pybind11.h>
+
 #include <iostream>
 #include <vector>
-
+#include <string>
 #include <opencv2/opencv.hpp> 
 
 
-int main(int argc, char **argv){
+
+using namespace std;
+
+void run_test(int argc, char **argv){
 
 	string filepath = string("/Users/will/projects/cpprtx/assets/images/grass.jpg");
 	cv::Mat img = imread(filepath, cv::IMREAD_COLOR);
 	cv::resize(img, img, cv::Size(1024,1024), 0, 0, cv::INTER_LINEAR);
 
-	return 0;
+	cout << "Yolo" << endl;
 }
 
-/*
+
 PYBIND11_MODULE(test_OPENCV, m) {
     m.doc() = "Testing basic load cv mat";
     m.def("runtest", &runtest, "Run load image test");
 }
-*/
